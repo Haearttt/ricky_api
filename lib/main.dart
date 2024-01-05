45ff113d41096fdce86bf25ad8fcf42a78807b7d
Hidayet <sight066@gmail.com>
2024-01-05 14:45:19 +0300

    hgvc

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,18 @@ import 'package:ricky/view/main_page.dart';
 import 'package:ricky/view_model/main_view_model.dart';
 
 void main() {
-  runApp( MainApp());
+  runApp(const MainApp());
 }
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: ChangeNotifierProvider(
         create: (BuildContext context) => MainViewModel(),
-        child: MainPage(),
+        child: const MainPage(),
       ),
     );
   }
